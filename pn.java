4c47bf20be5ead5828ea36b127cabbdcab065182
Santhoshsanto <40232154+Santhoshsanto@users.noreply.github.com>
2019-02-28 02:15:34 -0800

    Create pn.java

---
+++ pn.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class array1 {
+
+	    public static void main(String[] args) {
+	    	Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+
+	        if (n < 0.0)
+	            System.out.println(n + " is a negative number.");
+
+	     else if ( n > 0.0)
+	            System.out.println(n + " is a positive number.");
+	        else
+	            System.out.println(n + " is 0.");
+	    }
+	}
